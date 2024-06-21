8d130061680fc5f6e89e205f3630eade8f5cccc6
dev-atulyadav <atulyadav031103@gmail.com>
2024-06-22 03:47:08 +0530

    login form created

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,4 +28,11 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
-export { auth, googleProvider, RecaptchaVerifier, signInWithPhoneNumber };
+export {
+  auth,
+  googleProvider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+  signInWithEmailAndPassword,
+};
--- src/components/Forms/Login.jsx
+++ src/components/Forms/Login.jsx
@@ -1,9 +1,190 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  auth,
+  signInWithEmailAndPassword,
+  googleProvider,
+  signInWithPopup,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
+} from "../../../firebaseConfig";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
+  const [confirmationResult, setConfirmationResult] = useState(null);
+  const [isLoginWithPhone, setIsLoginWithPhone] = useState(false);
 
-export default Login
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Login successful");
+    } catch (error) {
+      alert("Error logging in: " + error.message);
+    }
+  };
+
+  const handleGoogleLogin = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+      alert("Login successful");
+    } catch (error) {
+      alert("Error logging in: " + error.message);
+    }
+  };
+
+  const setupRecaptcha = () => {
+    window.recaptchaVerifier = new RecaptchaVerifier(
+      "recaptcha-container",
+      {
+        size: "invisible",
+        callback: (response) => {
+          // reCAPTCHA solved - will proceed with submit function
+          handlePhoneLogin();
+        },
+      },
+      auth
+    );
+  };
+
+  const handlePhoneLogin = async (e) => {
+    e.preventDefault();
+    setupRecaptcha();
+    const phoneNumber = "+1" + phone;
+    const appVerifier = window.recaptchaVerifier;
+    try {
+      const confirmationResult = await signInWithPhoneNumber(
+        auth,
+        phoneNumber,
+        appVerifier
+      );
+      setConfirmationResult(confirmationResult);
+      alert("OTP has been sent");
+    } catch (error) {
+      alert("Error logging in: " + error.message);
+    }
+  };
+
+  const verifyOtp = async (e) => {
+    e.preventDefault();
+    try {
+      await confirmationResult.confirm(otp);
+      alert("Phone Login successful");
+    } catch (error) {
+      alert("Error verifying OTP: " + error.message);
+    }
+  };
+
+  return (
+    <section className=" top-0 absolute flex justify-center items-center h-screen w-full bg-[#b8b8b856] backdrop-blur-[3px] z-40 ">
+      <article className=" p-6 rounded-2xl shadow-xl shadow-[#0000005d] bg-white">
+        <h2 className="text-3xl font-bold mb-4">Login</h2>
+        <main>
+          {!isLoginWithPhone ? (
+            <form
+              className="flex flex-col justify-center items-center gap-6"
+              onSubmit={handleLogin}
+            >
+              <div className="flex justify-start flex-col gap-3">
+                <label htmlFor="email">Email:</label>
+                <input
+                  className="px-4 h-12 w-72 rounded-lg border border-black focus:border-transparent"
+                  placeholder="Enter Email"
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="flex justify-start flex-col gap-3">
+                <label htmlFor="password">Password:</label>
+                <input
+                  className="px-4 h-12 w-72 rounded-lg border border-black focus:border-transparent"
+                  placeholder="Enter Password"
+                  type="password"
+                  id="password"
+                  name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                className="px-3 w-24 uppercase h-12 border-sky-400 border rounded-lg text-sky-500 font-semibold hover:bg-sky-500 hover:text-white duration-200"
+                type="submit"
+              >
+                Login
+              </button>
+            </form>
+          ) : (
+            <form
+              className="flex flex-col justify-center items-center gap-6"
+              onSubmit={handlePhoneLogin}
+            >
+              <div id="recaptcha-container"></div>
+              <div className="flex justify-start flex-col gap-3">
+                <label htmlFor="phone">Phone:</label>
+                <input
+                  className="px-4 h-12 w-72 rounded-lg border border-black focus:border-transparent"
+                  placeholder="Enter Phone Number"
+                  type="tel"
+                  id="phone"
+                  name="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="px-3 w-32 uppercase h-12 border-sky-400 border rounded-lg text-sky-500 font-semibold hover:bg-sky-500 hover:text-white duration-200"
+              >
+                Send OTP
+              </button>
+            </form>
+          )}
+          <div className="flex justify-center items-center flex-col gap-4 mt-4 border-y border-black p-2">
+            <span>Or</span>
+            <div className="flex justify-center items-center flex-col gap-4">
+              <button
+                className="px-3 py-2 bg-white rounded-full border border-yellow-400 hover:bg-yellow-400 hover:text-white duration-200 text-yellow-400"
+                onClick={handleGoogleLogin}
+              >
+                Continue with Google
+              </button>
+              <button
+                className="px-3 py-2 bg-white rounded-full border border-red-400 hover:bg-red-400 hover:text-white duration-200 text-red-400 mb-2"
+                onClick={() => setIsLoginWithPhone(!isLoginWithPhone)}
+              >
+                Continue with {isLoginWithPhone ? "Email" : "Phone"}
+              </button>
+            </div>
+            {confirmationResult && (
+              <form
+                className="flex flex-col justify-center items-center gap-6"
+                onSubmit={verifyOtp}
+              >
+                <div className="flex justify-start flex-col gap-3">
+                  <label htmlFor="otp">OTP:</label>
+                  <input
+                    className="px-4 h-12 w-72 rounded-lg"
+                    placeholder="Enter OTP"
+                    type="text"
+                    id="otp"
+                    name="otp"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                  />
+                </div>
+                <input type="submit" value="Verify OTP" />
+              </form>
+            )}
+          </div>
+        </main>
+      </article>
+    </section>
+  );
+};
+
+export default Login;
